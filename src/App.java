9e18bd844c16bf84ae7304cd2319fe8a539281ad
marcamichelle1209-arch <marcamichelle1209@gmail.com>
2026-04-14 19:37:53 -0500

    UPD: App file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int [] array = new int[]{1,2,3,4,5};
+        System.out.println(array[0]);
+        System.out.println(array);
+        // for normal
+        for (int i= 0; i < array.length; i++) {
+            System.err.println(array[i]);
+        }
+        //for each
+        for( int i : array){
+            System.out.println(i);
+        }
     }
 }
